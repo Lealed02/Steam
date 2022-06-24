64ca59195de066e1dda33c569267a251d3d917d4
Lewis <Lewis@LAPTOP-O111IV05>
2022-06-25 00:44:35 +0100

    bed

--- steam4/src/Steam.java
+++ steam4/src/Steam.java
@@ -9,7 +9,15 @@ import javax.swing.border.*;
 
 public class Steam {
 	
+	//Images
+	private ImageIcon ALeft = new ImageIcon("left.png");
+	private ImageIcon ARight = new ImageIcon("right.png");
+	private ImageIcon iHome =  new ImageIcon(new ImageIcon("home.png").getImage().getScaledInstance(210, 32, Image.SCALE_DEFAULT));
+	private ImageIcon iSort = new ImageIcon("sort.png");
+	private ImageIcon iReady = new ImageIcon("ready.png");
+	private ImageIcon iCollections = new ImageIcon("collections.png");
 	
+	//
 	
 	private TopMenu tmenu = new TopMenu();
 	private BotMenu bMenu = new BotMenu();
@@ -21,8 +29,8 @@ public class Steam {
 	
 		private JLabel homeLabel = new JLabel();
 		private JScrollPane side; //Will turn sideBar into a scroll pane once initialised
-		private JFrame home = new JFrame();
-		private JFrame search = new JFrame();
+		private JPanel home = new JPanel();
+		private JPanel search = new JPanel();
 		private STop top = new STop(); //The top of the side bar
 		private SBot bot = new SBot();
 		
@@ -43,16 +51,28 @@ public class Steam {
             @Override
             public void run() {
             	JFrame frame = new JFrame();
-            	
+            	setBorders();
             	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             	frame.setMinimumSize(frameSize);
             	frame.add(mainFrame);
-            	
+            	frame.setJMenuBar(tmenu);
             	
             	frame.pack();
             	frame.setVisible(true);
             }
         });
+	}
+	
+	public void setBorders() {
+		BorderLayout std = new BorderLayout();
+		
+		tmenu.setBorder(BorderFactory.createEmptyBorder());
+		bMenu.setBorder(BorderFactory.createEmptyBorder());
+		top.setBorder(BorderFactory.createEmptyBorder());
+		bot.setBorder(BorderFactory.createEmptyBorder());
+		sideBar.setBorder(BorderFactory.createEmptyBorder());
+		//side.setBorder(BorderFactory.createEmptyBorder());
+		center.setBorder(BorderFactory.createEmptyBorder());
 	}
 	
 	
@@ -61,8 +81,12 @@ public class Steam {
 	
 	public class MainFrame extends JPanel {
 		public MainFrame() {
+			
+		
+		
 			this.setLayout(new BorderLayout());
 			
+			this.add(bMenu, BorderLayout.NORTH);
 			this.add(center, BorderLayout.CENTER);
 			this.add(sideBar, BorderLayout.WEST);
 			
@@ -72,23 +96,64 @@ public class Steam {
 	
 	public class TopMenu extends JMenuBar {
 		public TopMenu() {
-			this.setLayout(new BorderLayout());
-			this.add(home, BorderLayout.NORTH);
-			this.add(search, BorderLayout.CENTER);
+			
+			
+			JMenu steam= new JMenu("<html><font color='white'>Steam</font></html>");
+			JMenu view= new JMenu("<html><font color='white'>View</font></html>");
+			JMenu friends = new JMenu("<html><font color='white'>Friends</font></html>");
+			
+			
+			
+			this.add(steam);
+			this.add(view);
+			this.add(friends);
+			
+			this.setForeground(Color.WHITE);
+			
+			this.setBackground(Modes.Chmenu.toColor());
 		}
 	}
 	
 	
 	public class BotMenu extends JMenuBar {
 		public BotMenu()  {
+			Font big = new Font("FF Din OT Bold", Font.PLAIN, 20);
 			
+			
+			 JMenu menuStart= new JMenu("<html><font color='white'>STORE</font></html>");
+			 JMenu menuEdit= new JMenu("<html><font color='white'>LIBRARY</font></html>");
+			 JMenu menuWindow= new JMenu("<html><font color='white'>COMMUNITY</font></html>");
+			 JMenu menuHelp= new JMenu("<html><font color='white'>PUT THE BUTTER...</font></html>");
+			 JMenu menuBack = new JMenu();
+			 JMenu menuFront = new JMenu();
+			 
+			 menuEdit.setFont(big);
+			 menuStart.setFont(big);
+			 menuWindow.setFont(big);
+			 menuHelp.setFont(big);
+			 
+			 
+			 menuBack.setIcon(ALeft);
+			 menuFront.setIcon(ARight);
+			 
+			 
+			 	this.add(menuBack);
+			 	this.add(menuFront);
+			 	this.add(menuStart);
+			 	this.add(menuEdit);
+			 	this.add(menuWindow);
+			 	this.add(menuHelp);
+			    
+			    
+			 	this.setBackground(Modes.Clmenu.toColor());
+			 	this.setBorder(BorderFactory.createRaisedBevelBorder());
 		}
 		
 	}
 	
 	public class FCenter extends JPanel{
 		public FCenter() {
-			this.setBackground(Color.darkGray);
+			this.setBackground(Color.lightGray);
 		}
 	}
 	
@@ -97,7 +162,7 @@ public class Steam {
 			this.setLayout(new BorderLayout());
 			this.setBackground(Color.red);
 			this.setMinimumSize(new Dimension(300,300));
-			
+		
 			this.add(top, BorderLayout.NORTH);
 			this.add(bot, BorderLayout.CENTER);
 			
@@ -111,9 +176,29 @@ public class Steam {
 	
 	public class STop extends JPanel {
 		public STop() {
-			FlowLayout f1 = new FlowLayout(FlowLayout.LEADING);
+			this.setLayout(new BorderLayout());
 			this.setBackground(Modes.Clmenu.toColor());
+			iHome =  new ImageIcon(new ImageIcon("home.png").getImage().getScaledInstance(210, 32, Image.SCALE_DEFAULT));
+			setupHome();
+			this.add(home, BorderLayout.NORTH);
+			this.add(search, BorderLayout.CENTER);
+		}
+		
+		public void setupHome() {
+			FlowLayout f1 = new FlowLayout(FlowLayout.LEADING);
 			
+			home.setLayout(f1);
+			home.setBackground(Modes.Clmenu.toColor());
+			
+			homeLabel.setIcon(iHome);
+			home.add(homeLabel);
+			
+			JLabel collections = new JLabel();
+			collections.setIcon(iCollections);
+			home.add(collections);
+		}
+		
+		public void setupSearch() {
 			
 		}
 	}
@@ -134,16 +219,11 @@ public class Steam {
 	
 	
 	//Images
-	private ImageIcon ALeft = new ImageIcon("left.png");
-	private ImageIcon ARight = new ImageIcon("right.png");
-	private ImageIcon iHome =  new ImageIcon(new ImageIcon("home.png").getImage().getScaledInstance(210, 32, Image.SCALE_DEFAULT));
-	private ImageIcon iSort = new ImageIcon("sort.png");
-	private ImageIcon iReady = new ImageIcon("ready.png");
-	private ImageIcon iCollections = new ImageIcon("collections.png");
+
 	
 	//Colors
 	public enum Modes {
-	    Chmenu (new Color(21,46,71,255)),
+	    Chmenu (new Color(28,40,55,255)),
 	     Clmenu(new Color(30,32,38,255)),
 	    Ctop (new Color(1,1,1)),
 	    Cbot (new Color(36,40,47,255)),
